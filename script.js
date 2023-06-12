72a6dadbf0cb4c330d71da1670531fb00ed89473
Wilson Siaw <siawwils90@gmail.com>
2023-06-12 16:48:31 +0200

    Create a function that adds to the mylibrary array

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 let myLibrary = [];
 
-function Book(title, author, year, publisher) {
+function Book(title, author, pages, year, publisher) {
     this.title = title
     this.author = author
+    this.pages = pages
     this.year = year
     this.publisher = publisher
 }
+
+function addBookToLibrary() {
+    myLibrary.push();
+}
+
+addBookToLibrary();
 
